01a7115425eb27c7dd5292d15b28426626a9da8c
mathausdev <114772818+mathausdev@users.noreply.github.com>
2023-01-28 00:54:54 -0300

    Add files via upload

--- WhatsApp.js
+++ WhatsApp.js
@@ -1,7 +1,7 @@
 
     var whatsappButton = document.getElementById("whatsapp-button");
     var phoneNumber = "31992326226"; // Número de telefone com DDD
-    var message = "Olá, gostaria de mais informações?" // Mensagem padrão
+    var message = "Olá, gostaria de mais informações" // Mensagem padrão
 
     // Criar o botão
     var button = document.createElement("a");
@@ -18,3 +18,24 @@
 
     // Adicionar o botão ao HTML
     whatsappButton.appendChild(button);
+
+
+
+    // Add an event listener to the form
+var form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // Get the input fields
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  // Check if all fields are filled
+  if (name === "" || email === "" || message === "") {
+    alert("Por favor, preencha todos os campos!");
+    return;
+  }
+
+  // Continue with sending the form data
+});
